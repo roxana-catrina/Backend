d622d66fb19f882f85d988d410072502d1a6d693
roxana-catrina <roxanacatrina22@gmail.com>
2025-11-22 12:57:30 +0200

    mesaj functional

---
+++ Licenta/src/main/java/Licenta/Licenta/Controller/WebSocketMesajController.java
@@ -0,0 +1,38 @@
+package Licenta.Licenta.Controller;
+
+import Licenta.Licenta.Dto.MesajDTO;
+import Licenta.Licenta.Service.MesajService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.security.Principal;
+import java.util.Map;
+
+@Controller
+@CrossOrigin(origins = "http://localhost:4200")
+public class WebSocketMesajController {
+
+    @Autowired
+    private MesajService mesajService;
+
+    @MessageMapping("/chat.send")
+    @SendToUser("/queue/messages")
+    public MesajDTO sendMessage(@Payload MesajRequest request,
+                                Principal principal) {
+        return mesajService.trimiteMesaj(
+                request.getExpeditorId(),
+                request.getDestinatarId(),
+                request.getContinut()
+        );
+    }
+
+    @MessageMapping("/chat.typing")
+    @SendToUser("/queue/typing")
+    public Map<String, Object> userTyping(@Payload Map<String, Object> payload) {
+        return payload;
+    }
+}
